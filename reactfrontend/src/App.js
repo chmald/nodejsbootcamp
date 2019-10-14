78eb13e6551c5f2ca46e328b5164d1cb2191c0f6
Christopher Maldonado <chmald@microsoft.com>
2019-10-14 16:36:09 -0500

    react

---
+++ reactfrontend/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3000/users').then(
+      res => {
+        const users = res.data;
+        this.setState({ users });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>Our Users</h3>
+          <p>
+            {this.state.users.map(user => <li>{user.user} - {user.job}</li>)}
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
