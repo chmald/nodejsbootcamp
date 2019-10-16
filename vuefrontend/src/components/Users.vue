f880773d4b775362edb2c6595c65930efa1cbe9f
Christopher Maldonado <chmald@microsoft.com>
2019-10-16 12:50:16 -0500

    vue

---
+++ vuefrontend/src/components/Users.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <li :key="user.user" v-for="user in users">
+            {{ user.user }} - {{ user.job }}
+        </li>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    mounted() {
+        axios.get('http://localhost:3000/users').then(
+            res => {
+                this.users = res.data;
+        });
+    }
+}
+</script>
+
+<style>
+</style>
