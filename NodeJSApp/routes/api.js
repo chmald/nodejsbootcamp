8422b54ebf049fb1b1c575b406ce171120132fb4
Christopher Maldonado <chmald@microsoft.com>
2019-10-24 13:43:39 -0500

    delete nodejsapp

--- NodeJSApp/routes/api.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.use('/users', require('./users'));
-router.use('/marketing', require('./marketing'));
-router.use('/reports', require('./reports'));
-router.use('/admin', require('./admin'));
-
-module.exports = router;
--- NodeJSApp/routes/reportchild.js
+++
@@ -1,4 +0,0 @@
-reportnum = 1;
-for (let index = 1; index < 2147483647; index++) {
-    reportnum = reportnum * index;
-}
